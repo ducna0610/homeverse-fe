39653e9a96e529d6e9d12153f89e33a83c6b31ef
ducna0610 <ducna0610@gmail.com>
2024-10-03 15:06:33 +0700

    feat: add page about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './layouts/user/user.component';
+import { AboutComponent } from './pages/user/about/about.component';
+import { ContactComponent } from './pages/user/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: UserComponent,
+        children: [
+            {
+                path: 'about',
+                component: AboutComponent,
+            },
+        ],
+    },
+];
